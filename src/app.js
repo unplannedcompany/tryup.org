1471046ffd90074c016feebf517cc4938b932be3
Daniel Miller <daniel@wants.email>
2016-10-04 22:31:45 -0500

    Handle navigating backward from another page

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import './style/syncingAnimation.scss'
 
 import configureEditor from './configureEditor'
 
-
 const getElementById = document.getElementById.bind(document)
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,3 +15,16 @@ document.addEventListener('DOMContentLoaded', () => {
     getElementById('document-container'),
     getElementById('table-of-contents-container'))
 })
+
+window.addEventListener('pageshow', function() {
+  // When the user navigates back to our page, the browser resets the scroll
+  // position of our scrollable document panel.
+  //
+  // Assigning `window.location.href` to itself causes the browser to
+  // re-scroll to the hash location in the URL. This isn't a perfect solution,
+  // because it merely scrolls the panel back to the last internal link the user
+  // followed.
+  if (window.location.hash) {
+    window.location.href = window.location.href
+  }
+});
