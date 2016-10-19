ab75895d0d14c2019fa0419ed24e43c1560d861e
Daniel Miller <daniel@wants.email>
2016-10-19 09:51:06 -0500

    Slightly refactor tab control configuration

--- src/configureTabControls.js
+++ src/configureTabControls.js
@@ -7,28 +7,26 @@ export default function configureTabControls(tabContainer, tabPanelContainer) {
   // Right now, we just have two tabs: one for showing the table of contents,
   // and one for showing the documentation itself.
   for (let tab of tabs) {
-    configureTab(tab, tabs, tabPanels)
+    tab.addEventListener('click', () => selectTab(tab, tabs, tabPanels))
   }
 }
 
-function configureTab(tab, tabs, tabPanels) {
-  tab.addEventListener('click', () => {
-    // First, let's mark every tab as unselected
-    for (let i = 0; i < tabs.length; i++) {
-      setSelectedAttribute(tabs[i], 'false')
-    }
+function selectTab(tab, tabs, tabPanels) {
+  // First, let's mark every tab as unselected
+  for (let i = 0; i < tabs.length; i++) {
+    setSelectedAttribute(tabs[i], 'false')
+  }
 
-    // Next, let's hide every tab panel
-    for (let i = 0; i < tabPanels.length; i++) {
-      tabPanels[i].style.display = 'none'
-    }
+  // Next, let's hide every tab panel
+  for (let i = 0; i < tabPanels.length; i++) {
+    tabPanels[i].style.display = 'none'
+  }
 
-    // Now, we'll mark the tab the user just clicked as selected
-    setSelectedAttribute(tab, 'true')
+  // Now, we'll mark the tab the user just clicked as selected
+  setSelectedAttribute(tab, 'true')
 
-    // And finally, we'll show the associated table panel 
-    getElementById(tab.getAttribute('aria-controls')).style.display = 'block'
-  })
+  // And finally, we'll show the associated table panel 
+  getElementById(tab.getAttribute('aria-controls')).style.display = 'block'
 }
 
 function setSelectedAttribute(tab, value) {
