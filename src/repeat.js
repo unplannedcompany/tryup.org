457e5ba10c046f1148b751aae93547a3f55fa84a
Daniel Miller <daniel@wants.email>
2016-11-06 12:33:42 -0600

    Remove padding for table captions

--- src/repeat.js
+++ src/repeat.js
@@ -1,4 +1,4 @@
-// Returns a new string consisting of `count` copies of `text`
+// Returns a new string consisting of `count` copies of `text`.
 export default function repeat(text, count) {
   return new Array(count + 1).join(text)
 }
