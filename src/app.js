9643794c94f5e792a679887ec6358962b0ee6dc6
Daniel Miller <daniel@wants.email>
2016-11-12 15:46:13 -0600

    Make elements w/ tab index clickable w/ keyboard

--- src/app.js
+++ src/app.js
@@ -20,4 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
   configureTableOfContentsVisibility(
     getElementById('show-documentation'),
     getElementById('show-table-of-contents'))
+
+  // If an element has an explicit tab index, it's meant to be interactable with a keyboard.
+  //
+  // Here, we make these elements clickable using the enter key or space bar.
+  documentationContainer.addEventListener('keydown', event => {
+    const element = event.target
+    const ENTER_KEY = 13
+    const SPACE_KEY = 32
+
+    if (element.hasAttribute('tabindex') && [ENTER_KEY, SPACE_KEY].indexOf(event.keyCode) !== -1) {
+      event.preventDefault()
+      element.click()
+    }
+  })
 })
