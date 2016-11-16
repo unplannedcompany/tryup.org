632c3585151953b3378a6ae8f5b66f16b7ecb845
Daniel Miller <daniel@wants.email>
2016-11-16 01:37:22 -0600

    Clarify isOnIosDevice function

--- src/isOnIosDevice.js
+++ src/isOnIosDevice.js
@@ -1,5 +1,4 @@
 // Returns `true` on an iPhone, iPad, or iPaod.
 export default function isOnIosDevice() {
-  // Based on this answer: http://stackoverflow.com/a/9039885/5667767
-  return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  return ['iPhone', 'iPad', 'iPod'].indexOf(navigator.platform) !== -1
 }
