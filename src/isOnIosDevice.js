111462b3f160ea62f128a6be645e6d8fb0d26481
Daniel <daniel@wants.email>
2017-01-12 17:41:26 -0600

    Fix typo in comment

--- src/isOnIosDevice.js
+++ src/isOnIosDevice.js
@@ -1,4 +1,4 @@
-// Returns `true` on an iPhone, iPad, or iPaod.
+// Returns `true` on an iPhone, iPad, or iPod.
 export default function isOnIosDevice() {
   return ['iPhone', 'iPad', 'iPod'].indexOf(navigator.platform) !== -1
 }
